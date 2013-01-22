6077378e35c79c552c866978efcaf6b882cfad54
Americo Savinon <americos@gmail.com>
2013-01-22 13:01:13 -0500

    Finishing Feature#1 for AT&T, adding README and apps needed

---
+++ custom_app_wiring/README.txt
@@ -0,0 +1,8 @@
+Instructions
+==================
+
+1) Import the CA_BRANCHES_APP into Presto
+2) Import att_sample_suscriber into Presto
+3) Import the att_sample_app_recieves_param into Presto
+4) In Mashboard drag the CA_BRANCHES_APP and the att_sample_suscriber
+5) When clicking on a bar in CA_BRANCHES_APP it will open a new tab showing the att_sample_app_receives_param
--- custom_app_wiring/att_sample_suscriber/js/app.js
+++ custom_app_wiring/att_sample_suscriber/js/app.js
@@ -1,8 +1,29 @@
 Sample.Hello = function( app ) {
-	
-	app.subscribe("americoTopic", function(topic, data){
-		jQuery("#data_display").html("Data was received! :" + data.data + " :: at: ");
-	 console.log("Data RECEIVED:", data);
-	});
+  
+  //app.subscribe("americoTopic", function(topic, data){
+  app.subscribe("BarChart.element-click.CA_Branches_APP", function(topic, data){
+    
+    //Branch comes like: "Miami #01" so lets remove that space and encode the hash.
+    var branch = data.Branch.replace(/ #/, "%23");  //HTML Escape code for # is %23
+    
+    var appContainer = app.getContainer ? app.getContainer() : undefined;
+    if(appContainer){
+      tabPanel = appContainer.findParentByType("tabpanel");
+           if(tabPanel){
+               tabPanel.loadTab({
+                   title : "Drill down",
+                   xtype: 'ema-htmlpanel',
+                   closable: true,
+                   url: "http://prestocloud.jackbe.com/presto/hub/applauncher.html?mid=att_sample_app_recieves_param&city=" + branch
+               });
+             //return;
+           }
+    }
+    
+    jQuery("#data_display").html("Data was received! Branch is:" + branch);
+   
+   
+  });
 };
 
+​
